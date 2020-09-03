ecff6bcb15cd724a941c1e736fb6e2eea178079f
SandeepRavuri4 <sandeep.jashu1431@gmail.com>
2020-09-03 15:15:31 +0530

    added codes

---
+++ Sandeep/cprog/ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	printf("enter a char");
+	scanf("%c",&c);
+	printf("ascii val of :%c = %d",c,c);
+	return 0;
+}
---
+++ Sandeep/cprog/datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	char c;
+	double d;
+	printf("enter integer val:\n float value:\n char val:\n double val:\n");
+	scanf("%d %f %c %lf",&a,&b,&c,&d);
+	printf("integer =%d\n float = %f\n char =%c\n double = %lf\n",a,b,c,d);
+}
+
