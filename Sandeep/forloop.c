444dd9c1b5d427a6417a4a8700183e5499e22ccc
SandeepRavuri4 <sandeep.jashu1431@gmail.com>
2020-09-03 11:23:27 +0530

    commit msg

---
+++ Sandeep/forloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	printf("enter a num:");
+	scanf("%d",&a);
+	for(i=1;i<=a;i++)
+	{
+		printf("git hub");
+	}
+	return 0;
+}
+
---
+++ loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	printf("enter a number");
+	scanf("%d",&a);
+	for(i=0;i<a;i++)
+	{
+		printf("GITHUB");
+	}
+	return 0;
+}
+
