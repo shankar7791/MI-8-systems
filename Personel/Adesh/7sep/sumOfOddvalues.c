de983bca3e5d26242c7dac3f58915d7bf3994b73
AdeshK1331 <70565960+AdeshK1331@users.noreply.github.com>
2020-09-07 20:24:37 +0530

    Update sumOfOddvalues.c

--- Personel/Adesh/7sep/sumOfOddvalues.c
+++ Personel/Adesh/7sep/sumOfOddvalues.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 int 
 main(){
-	int i, num[5], sum=0;
-	printf("\nInput the 1st number: "); 
-    scanf("%d", &num[0]);
-    printf("\nInput the 2nd number: "); 
-    scanf("%d", &num[1]);
-    printf("\nInput the 3rd number: "); 
-    scanf("%d", &num[2]);
-	printf("\nInput the 4th number: "); 
-    scanf("%d", &num[3]);
-    printf("\nInput the 5th number: "); 
-    scanf("%d", &num[4]);
-	for(i = 0; i < 5; i++) {
+	int i, num[10], sum=0;
+	int n;
+	printf("Enter how many number you want?");
+	scanf("%d",&n);
+	for(i = 0; i < n; i++)
+		{
+		  printf("\nInput the %d number: ",i); 
+    		  scanf("%d", &num[i]);
+		}
+	for(i = 0; i < n; i++) {
 		if((num[i]%2) != 0) 
 		{
 		   sum += num[i];
