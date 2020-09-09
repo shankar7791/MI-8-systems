a3b4031a3115eb7a3755fcecfbd524d3b90db88b
joshivaibhav1196 <joshivaibhav1196@gmail.com>
2020-09-09 15:54:22 +0530

    uploading prob9.c

---
+++ Personel/Vaibhav/problem_solving/09-09-2020/prob9.c
@@ -0,0 +1,19 @@
+/* write c program to reverse given number */
+
+#include <stdio.h>
+int 
+main() 
+{
+    int n, rev = 0, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    while (n != 0) 
+    {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    printf("Reversed number = %d", rev);
+    return 0;
+}
+
