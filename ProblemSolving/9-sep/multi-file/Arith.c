63c387138c658debf5d5402d88eaaed98d34420e
shankar7791 <shankar7791@gmail.com>
2020-09-10 14:20:56 +0530

    uploading 10-sep 9-problems

---
+++ ProblemSolving/9-sep/multi-file/Arith.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern void add(int a, int b);
+
+int main()
+{
+	int a = 10, b = 20;
+	
+	add(a, b);
+
+	return 0;
+
+	
+}
---
+++ ProblemSolving/9-sep/multi-file/add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern void add(int x, int y)
+{
+	int ans = x + y;
+	
+	printf("\n addition = %d", ans);
+}
