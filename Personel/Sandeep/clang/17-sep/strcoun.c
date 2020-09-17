f6666f541df2d9580cef20b12210a65f52d92bb9
SandeepRavuri4 <sandeep.jashu1431@gmail.com>
2020-09-17 16:05:20 +0530

    san@work:

---
+++ Personel/Sandeep/clang/17-sep/strcoun.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+   char str[100];
+   int c = 0, co[26] = {0}, x;
+
+   printf("Enter a string\n");
+   gets(str);
+
+   while (str[c] != '\0') 
+   {
+      if (str[c] >= 'a' && str[c] <= 'z')
+      {
+         x = str[c] - 'a';
+         co[x]++;
+      }
+      c++;
+    }
+
+   for (c = 0; c < 26; c++)
+         printf("%c occurs %d times in the string.\n", c + 'a', co[c]);
+
+   return 0;
+}
