874d24b62faeaaf4d6c9886a49e26e3a7f94ee2b
hrushikesh30 <hrushikeshrasale007@gmail.com>
2020-09-21 11:36:58 -0700

    todays prob

---
+++ Personel/hrushikesh/problem_solving/sep21/prob2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+  char a[100];
+  int length;
+
+  printf("Enter a string to calculate its length\n");
+  gets(a);
+
+  length = strlen(a);
+
+  printf("Length of the string = %d\n", length);
+
+  return 0;
+}
