385b6ce7913a1a3aee7465270ba1f153914905be
AdeshK1331 <adeshkoffice1331@gmail.com>
2020-09-23 19:25:04 +0530

    Project

---
+++ Team_01/Project 1/sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int number_comparisons = 0;
+int number_swaps = 0;
+
+void swap(int *x, int *y)
+{
+    number_swaps++;
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+        {
+            number_comparisons++;
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d  ", arr[i]);
+    printf("\n");
+}
