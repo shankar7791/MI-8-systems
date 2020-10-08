8325a6e05c06248b28de7f6941e73b6e70355f02
AdeshK1331 <adeshkoffice1331@gmail.com>
2020-10-08 12:41:20 +0530

    cpp

---
+++ Personel/Adesh/CPP programming/8oct/string.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1("AmberSoft Tech ");
+
+    string str2(str1);
+
+    string str3(7, '@');
+
+    string str4(str1, 10, 4);
+    string str5(str2.begin(), str2.begin() + 10);
+
+    cout << endl;
+    cout << "str1 :"<< str1 << endl;
+    cout << "str2 :"<< str2 << endl;
+    cout << "str3 :"<< str3 << endl;
+    cout << "str4 :"<< str4 << endl;
+    cout << "str5 :"<< str5 << endl;
+    cout << endl;
+    cout << "Compare String 1 and 2\n" ;
+    if ( str1.compare(str2) == 0 ) 
+        cout << "Strings are equal\n"; 
+    else 
+        cout << "Strings are unequal\n";
+    cout << endl;
+
+    cout << "First occurrence of \"Tech\" starts from : ";
+    cout << str1.find("Tech") << endl;
+    cout << endl;
+
+    str2.insert(15, "Pune ");
+    cout << "Insert String: " << str2 << endl;
+    cout << endl;
+
+    str2.append("India");
+    cout << "Append String: " << str2 << endl;
+    cout << endl;
+    return 0;
+}
