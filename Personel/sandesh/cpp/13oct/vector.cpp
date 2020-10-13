e7c1ed1f2547f0053796983a1c494a4e66cd6ee6
sandesh2797 <sandesh2797@gmail.com>
2020-10-13 19:20:06 +0530

    vector

---
+++ Personel/sandesh/cpp/13oct/vector.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+int
+main()
+{
+   int ch, val;
+   vector<int> vec;
+   cout<<"1.Insert Element into the Vector"<<endl;
+   cout<<"2.Delete Last Element of the Vector"<<endl;
+   cout<<"3.Print size of the Vector"<<endl;
+   cout<<"4.Display Vector elements"<<endl;
+   cout<<"5.Clear the Vector"<<endl;
+   cout<<"6.Exit"<<endl;
+
+   do {
+      cout<<"Enter your Choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1:
+         cout<<"Enter value to be inserted: "<<endl;
+         cin>>val;
+         vec.push_back(val);
+         break;
+
+         case 2:
+         cout<<"Last Element is deleted."<<endl;
+         vec.pop_back();
+         break;
+
+         case 3:
+         cout<<"Size of Vector: ";
+         cout<<vec.size()<<endl;
+         break;
+
+         case 4:
+         cout<<"Displaying Vector Elements: ";
+         for (int i = 0; i < vec.size(); i++)
+         cout<<vec[i]<<" ";
+         cout<<endl;
+         break;
+
+         case 5:
+         vec.clear();
+         cout<<"Vector Cleared"<<endl;
+         break;
+
+         case 6:
+         cout<<"Exit"<<endl;
+         break;
+         default:
+         cout<<"invalid option"<<endl;
+      }
+   } while (ch!=6);
+   return 0;
+}
