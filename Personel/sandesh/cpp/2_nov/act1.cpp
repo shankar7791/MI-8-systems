604732ed9e8c32ae6df8bc4318602465da80d435
sandesh2797 <sandesh2797@gmail.com>
2020-11-03 16:36:39 +0530

    union

---
+++ Personel/sandesh/cpp/2_nov/act1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var;
+    int *ptr;
+    int **pptr;
+
+    var= 12345;
+
+    ptr= &var;
+
+    pptr= &ptr;
+
+    cout<<"value of var is"<<var<<endl;
+    cout<<"value of ptr is"<<ptr<<endl;
+    cout<<"value of dptr is"<<pptr<<endl;
+
+    return 0;
+
+}
