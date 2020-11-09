cf71f21d12b4035e3942018070b9cdb0060e49a9
sandesh2797 <sandesh2797@gmail.com>
2020-11-09 18:30:37 +0530

    odifi

--- Personel/sandesh/cpp/2_nov/act1.cpp
+++ Personel/sandesh/cpp/2_nov/act1.cpp
@@ -9,17 +9,12 @@ int main()
     int var;
     int *ptr;
     int **pptr;
-
     var= 12345;
-
     ptr= &var;
-
     pptr= &ptr;
 
-    cout<<"value of var is"<<var<<endl;
-    cout<<"value of ptr is"<<ptr<<endl;
-    cout<<"value of dptr is"<<pptr<<endl;
-
+    cout << "value of var is" << var << endl;
+    cout << "value of ptr is" << ptr << endl;
+    cout << "value of dptr is" << pptr << endl;
     return 0;
-
 }
