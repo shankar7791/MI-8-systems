0fc3aafe32e99103a7d7f8b495d1262ec8880e5e
Vishu210 <70758670+Vishu210@users.noreply.github.com>
2020-11-23 17:33:29 +0530

    23 Nov activities

---
+++ Personel/Vishwajeet/Daily activities/circular LL.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+struct Node {
+   int data;
+   struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int newdata) {
+   struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+   struct Node *ptr = head;
+   newnode->data = newdata;
+   newnode->next = head;
+   if (head!= NULL) {
+      while (ptr->next != head)
+      ptr = ptr->next;
+      ptr->next = newnode;
+   } else
+   newnode->next = newnode;
+   head = newnode;
+}
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   do {
+      cout<<ptr->data <<" ";
+      ptr = ptr->next;
+   } while(ptr != head);
+}
+int main() {
+   insert(98);
+   insert(45);
+   insert(86);
+   insert(73);
+   insert(56);
+   cout<<"The circular linked list is: ";
+   display();
+   return 0;
+}
