b591fcbbc91a490c5ce06c5e69813a5d83f9b716
sandesh2797 <sandesh2797@gmail.com>
2020-12-22 14:28:24 +0530

    changes

--- Personel/sandesh/project/stud.cpp
+++ Personel/sandesh/project/stud.cpp
@@ -1,63 +1,68 @@
 #include <iostream>
-#include<string.h>
+#include <string.h>
 using namespace std;
 
-struct Node 
- {
-   int id;
-   char name[50];
-   struct Node *prev;
-   struct Node *next;
+struct Node
+{
+    int id;
+    char name[50];
+    char add[50];
+    long mobile;
+    float per;     
+    struct Node *prev;
+    struct Node *next;
 };
 
-struct Node* head[9];
+int key;
+
+struct Node *head[9];
 void init()
 {
-   for (int i = 0; i <= 9; i++)
-      head[i] = NULL;
+    for (int i = 0; i <= 9; i++)
+        head[i] = NULL;
 }
+
 void insert(struct Node stud)
- {
-   int key = stud.id % 9;
+{
+     key = stud.id % 9;
 
-   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
-   newnode->id = stud.id;
-   strcpy(newnode->name,stud.name);
-   newnode->prev = NULL;
-   newnode->next = NULL;
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->id = stud.id;
+    strcpy(newnode->name, stud.name);
+    strcpy(newnode->add, stud.add);
+    newnode->mobile = stud.mobile;
+    newnode->per = stud.per;
+    newnode->prev = NULL;
+    newnode->next = NULL;
 
+    if (head[key] == NULL)
+    {
+        head[key] = newnode;
+        //newnode->next=
+    }
 
-   
-   if(head[key] == NULL)
-   {
-      head[key]= newnode;
-      //newnode->next=
-   }
-
-       else
+    else
     {
         //add the node at the end of chain[key].
         struct Node *temp = head[key];
-        while(temp->next)
+        while (temp->next)
         {
             temp = temp->next;
         }
 
-        temp->next = newNode;
+        temp->next = newnode;
         newnode->next = NULL;
-        newnode->prev=temp;
-
+        newnode->prev = temp;
     }
-  
 }
-// void display() 
+// void display()
 // {
 //    struct Node* ptr;
 //    ptr = head[key];
 //    while(ptr != NULL)
 //     {
-//       cout<< ptr->id <<" "<<endl;   
-//       cout<< ptr->name <<" "<<endl;   
+//       cout<< ptr->id <<" "<<endl;
+//       cout<< ptr->name <<" "<<endl;
 //       ptr = ptr->next;
 //    }
 // }
@@ -66,29 +71,43 @@ void display()
 {
     int i;
 
-    for(i = 0; i < size; i++)
+    for (i = 0; i < 9; i++)
     {
-        struct node *temp = chain[i];
-        printf("chain[%d]-->",i);
-        while(temp)
+        struct Node *temp = head[key];
+        cout << "head[" << i <<"]<-->";
+        while (temp)
         {
-            printf("%d -->",temp->data);
+            cout << " " << temp->id << " " << temp->name << " " << temp->add << " " << temp->mobile << " " << temp->per << "<-->" << endl;
             temp = temp->next;
+
         }
-        printf("NULL\n");
+        cout << "NULL" << endl;
     }
+}
 
 int main()
- {
-   init();
-   struct Node stud;
-   cout <<"enter the id : ";
-   cin >> stud.id;
-   cout <<"enter the name : ";
-   cin >> stud.name;
-   
-   insert(stud);
-   cout<<"The list is: "<<endl<<endl;
-   display();
-   return 0;
+{
+    init();
+    struct Node stud;
+
+    display();
+    cout << endl << endl << "enter the student id : ";
+    cin >> stud.id;
+
+    cout << "enter the student name : ";
+    cin >> stud.name;
+
+    cout << "enter the student add : ";
+    cin >> stud.add;
+
+    cout << "enter the mobile number : ";
+    cin >> stud.mobile;
+
+    cout << "enter the student percentage : ";
+    cin >> stud.per;
+
+    insert(stud);
+    cout << "The list is: " << endl << endl;
+    display();
+    return 0;
 }
