c5269056b892ebcd7dabf1d7198078aba18cfffd
sandesh2797 <sandesh2797@gmail.com>
2021-01-22 16:39:12 +0530

    python

---
+++ Personel/sandesh/python/even.py
@@ -0,0 +1,8 @@
+num = int(input("enter the number"))
+if(num % 2) == 0:
+    print("is even")
+else:
+    print("is odd")
+    
+
+    
---
+++ Personel/sandesh/python/leap.py
@@ -0,0 +1,7 @@
+year = int(input("enter the year"))
+if(year % 400 )== 0:
+    if (year % 100) ==0 :
+        else:
+            print("is not leap year")
+    else:
+        print("is year leap year")
